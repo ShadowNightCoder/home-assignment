5ea930a5e0eeca3f68123139a60919ea4f677307
ShadowNightCoder <barbar0608@gmail.com>
2026-05-08 15:28:30 +0300

    adding style to this project and adding the basic html soon adding the api

--- task 3/front-sql/src/app/app.ts
+++ task 3/front-sql/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { Footer } from './components/footer/footer';
+import { Header } from './components/header/header';
+import { Main } from './components/main/main';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Footer, Header, Main],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
